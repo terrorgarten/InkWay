b54ec036ce6440a929e4376c2cab00c0a2e31a3a
terrorgarten <matejkonopik@gmail.com>
2023-05-22 18:36:02 +0200

    Initial Commit

---
+++ InkWay/InkWayApp.swift
@@ -0,0 +1,17 @@
+//
+//  InkWayApp.swift
+//  InkWay
+//
+//  Created by terrorgarten on 22.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct InkWayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
